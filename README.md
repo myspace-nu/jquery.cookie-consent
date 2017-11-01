3cefd70e8d1d09817c9a871b96b568142061270a
myspace-nu <32058020+myspace-nu@users.noreply.github.com>
2017-11-01 14:54:32 +0100

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,82 @@
 # jquery.cookie-consent
+
+A simple jQuery plugin for requesting consent for cookie usage.
+
+## Installation
+
+Include script *after* the jQuery library
+
+    <script src="/path/to/jquery.cookie-consent.min.js"></script>
+
+## Usage
+
+Create a consent request (displayed as a bar at the top of the page) - Bare minimum
+
+	<script type="text/javascript">
+		$(document).ready(function() {
+		  $.cookieConsent();
+		});
+	</script>
+
+Or, create a consent request from your own markup
+
+	<script type="text/javascript">
+		$(document).ready(function() {
+			$('#cookieConsent').cookieConsent();
+		});
+	</script>
+	
+	<style type="text/css">
+		#cookieConsent{ background-color:white;text-align:center;display:none;position:fixed;z-index:65000;bottom:0px;width:100%;font-size:14px; }
+		#cookieConsent button.cookieAccept{ background:#090;color:white;border:none;border-radius:0.2em;margin:0.5em;padding:0.2em 0.5em 0.2em 0.5em;}
+	</style>
+	<div id="cookieConsent">Vi använder cookies. Om du fortsätter använda vår webbplats innebär det att du godkänner detta.<button class="cookieAccept">Jag förstår</button></div>
+	
+## Options
+**message** - The consent message you want to show
+
+    closeButton: 'This website uses cookies. By using this website you consent to our use of these cookies.'
+	
+**style** - Optional style for the consent message
+
+    style: 'color:green;'
+
+**consentMessage** - The text shown on the consent button
+
+	consentMessage: 'I understand'
+   
+**consentStyle** - Optional style for the consent button
+
+    consentStyle: 'font-weight:bold;'
+	
+**acceptClass** - Class name assigned to the consent button
+
+    acceptClass: 'cookieAccept'
+
+*Default: cookieAccept*
+
+**consentTime** - Consent time in days (hides the consent request for this long)
+
+    consentTime: 365 // Hide it for 1 year
+
+**storage** - Where should we save the consent information?
+
+    storage: "local" // cookie => cookie, local => localStorage, session => sessionStorate (not really useful...)
+
+*Default: cookie*
+
+**onInit** - Function called on init
+
+    onInit: function(){ alert('Ready to go'); }
+
+**onConsent** - Function called when consent is given
+
+    onConsent: function(){ alert('Thank you!'); }
+
+**testing** - Always show the consent request (for testing)
+
+    testing: true
+
+*Default: false*
+
+### Author: [Johan Johansson](https://github.com/myspace-nu)
---
+++ src/jquery.cookie-consent.js
@@ -0,0 +1,90 @@
+/*!
+ * jQuery Cookie consent plugin 0.9
+ * https://myspace.nu
+ *
+ * Copyright 2017 Johan Johansson
+ * Released under the MIT license
+ */
+(function($) {
+	$.cookie = function (key, value, options) {
+		if (arguments.length > 1){
+			options = $.extend({}, options);
+			if (value === null || value === undefined) {
+				options.expires = -1;
+			}
+			return (document.cookie = [
+				encodeURIComponent(key), '=', options.raw ? value : encodeURIComponent(value),
+				options.expires ? '; expires=' + options.expires.toUTCString() : '',
+				options.path ? '; path=' + options.path : '',
+				options.domain ? '; domain=' + options.domain : '',
+				options.secure ? '; secure' : ''
+			].join(''));
+		}
+		var pairs = document.cookie.split('; ');
+		for (var i = 0, pair; pair = pairs[i] && pairs[i].split('='); i++) {
+			if (decodeURIComponent(pair[0]) === key) return decodeURIComponent(pair[1] || '');
+		}
+		return null;
+	};
+	$.fn.cookieConsent = function(options) {
+		var settings = $.extend({
+			position: "static",
+		  	message: "This website uses cookies. By using this website you consent to our use of these cookies.",
+		  	style: "",
+		  	consentMessage: "I understand",
+		  	consentStyle: "",
+		  	acceptClass: "cookieAccept",
+		  	consentTime: 3650, // 10 years
+		  	storage: "cookie",
+		  	onInit: function(){ },
+			onConsent: function(){ },
+		  	onTemplate: function(){ console.log(this) },
+		  	testing: false
+		}, options);
+		var language = window.navigator.userLanguage || window.navigator.language;
+		settings.storage =
+		  	(settings.storage==='local'&&typeof(Storage)!=="undefined")?'local':
+	  		(settings.storage==='session'&&typeof(Storage)!=="undefined")?'session':
+			'cookie';
+		var consentedDate =
+			(settings.storage==='local')?parseInt(localStorage.getItem("cookiesConsentDate")):
+			(settings.storage==='session')?parseInt(sessionStorage.getItem("cookiesConsentDate")):
+			parseInt($.cookie('cookiesConsentDate'));
+		var elm =
+			(this.length)?
+				this:
+				$("<div>", { html:settings.message, style:"background-color:white;text-align:center;display:none;"+settings.style })
+				.append($("<button>",{
+					html:settings.consentMessage,
+					style:"background:#090;color:white;border:none;border-radius:0.2em;margin:0.5em;padding:0.2em 0.5em 0.2em 0.5em;"+settings.consentStyle,
+		  			class:settings.acceptClass}))
+				.prependTo($("body"));
+	  	settings.onInit.call(elm);
+		if(settings.testing || !consentedDate || consentedDate+(86400000*settings.consentTime) < new Date().getTime()){
+		  	$(elm).show();
+		} else {
+	  	  	$(elm).hide(); // Make sure to hide it if defined style does not do this.
+		}
+		elm.each(function() {
+			var thisElm = $(this);
+			$(this).prependTo($("body"));
+			$(this).find("button."+settings.acceptClass).click(function() {
+				if(settings.storage==='local'){
+					localStorage.setItem("cookiesConsentDate", new Date().getTime());
+				} else if(settings.storage==='session') {
+					sessionStorage.setItem("cookiesConsentDate", new Date().getTime());
+				} else {
+					$.cookie('cookiesConsentDate',new Date().getTime(),
+						{ expires: new Date(new Date().getTime()+(86400000*settings.consentTime))}
+					);
+				}
+				thisElm.slideUp();
+			  	settings.onConsent.call(thisElm);
+			});
+		});
+		return this;
+	};
+	$.cookieConsent = function(options) {
+		$.fn.cookieConsent(options);
+	};
+}(jQuery));
